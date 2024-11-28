ebfc9a5ea5d944f157a7453173b885299402ed80
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-28 19:51:18 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,13 +5,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': '/src', // Asegura que las rutas relativas se resuelvan correctamente
-    },
-  },
-  build: {
-    outDir: 'dist', // Directorio donde se generará la aplicación
-    rollupOptions: {
-      input: './index.html', // Asegura que el archivo de entrada sea index.html
+      '@': '/src',
     },
   },
 });
