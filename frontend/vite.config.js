66312c58e38590b62d2a6357b3a1656b3eb32c30
rail-23 <railgonzalesaldana603@gmail.com>
2024-11-28 19:25:04 -0400

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': '/src', // Facilita los imports desde `src`
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: './index.html', // Define explícitamente el archivo de entrada
+    },
+  },
+});
